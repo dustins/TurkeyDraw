819ce8998c71809146d09bb5818c16a43293195d
Dustin Sweigart <dustin@swigg.net>
2013-11-21 23:32:44 -0500

    turkey drawing working with basic controls

--- TurkeyDrawLite/NGMyScene.h
+++ TurkeyDrawLite/NGMyScene.h
@@ -1,6 +1,6 @@
 //
 //  NGMyScene.h
-//  TurkeyDrawLite
+//  TurkeyDraw
 //
 
 //  Copyright (c) 2013 Dustin Sweigart. All rights reserved.
@@ -9,5 +9,14 @@
 #import <SpriteKit/SpriteKit.h>
 
 @interface NGMyScene : SKScene
+
+@property (retain) SKTexture *texture;
+@property (atomic) BOOL shouldDraw;
+@property (atomic) int counter;
+@property (atomic) int turkeyCount;
+@property (atomic) int targetTurkeyCount;
+@property (retain) SKNode *turkeyNode;
+@property (retain) SKLabelNode *labelNode;
+@property (retain) SKLabelNode *labelTurkeyCount;
 
 @end
--- TurkeyDrawLite/NGMyScene.m
+++ TurkeyDrawLite/NGMyScene.m
@@ -1,8 +1,8 @@
 //
 //  NGMyScene.m
-//  TurkeyDrawLite
+//  TurkeyDraw
 //
-//  Created by Dustin Sweigart on 11/21/13.
+//  Created by Dustin Sweigart on 11/20/13.
 //  Copyright (c) 2013 Dustin Sweigart. All rights reserved.
 //
 
@@ -10,44 +10,129 @@
 
 @implementation NGMyScene
 
+@synthesize texture;
+@synthesize shouldDraw;
+@synthesize counter;
+@synthesize turkeyNode;
+@synthesize turkeyCount;
+@synthesize targetTurkeyCount;
+@synthesize labelNode;
+@synthesize labelTurkeyCount;
+
 -(id)initWithSize:(CGSize)size {    
-    if (self = [super initWithSize:size]) {
-        /* Setup your scene here */
-        
+    if (self = [super initWithSize:size]) {        
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        turkeyNode = [SKNode node];
+        [self addChild:turkeyNode];
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
+        shouldDraw = false;
+        turkeyCount = 0;
+        targetTurkeyCount = 100;
+        
+        labelNode = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        labelNode.text = @"";
+        labelNode.fontSize = 16;
+        labelNode.position = CGPointMake(CGRectGetMidX(self.frame),
                                        CGRectGetMidY(self.frame));
+        [self addChild:self.labelNode];
         
-        [self addChild:myLabel];
+        labelTurkeyCount = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        labelTurkeyCount.text = [NSString stringWithFormat:@"%i tps", targetTurkeyCount];
+        labelTurkeyCount.fontSize = 12;
+        labelTurkeyCount.horizontalAlignmentMode = SKLabelHorizontalAlignmentModeLeft;
+        [self addChild:labelTurkeyCount];
+        
+        texture = [SKTexture textureWithImageNamed:@"turkey"];
     }
+    
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+- (void)addTurkeyNode {
+    if (!shouldDraw) {
+        return;
     }
+    
+    for (int i=0; i<(targetTurkeyCount-turkeyCount); i++) {
+        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:texture];
+        sprite = [self randomize:sprite];
+        
+        [self performSelector:@selector(addSprite:) withObject:sprite afterDelay:(float)rand()/RAND_MAX*1];
+        
+        turkeyCount++;
+    }
+    
+    [self performSelector:@selector(addTurkeyNode) withObject:Nil afterDelay:1];
+}
+
+- (void)addSprite:(SKNode *)sprite {
+    [self.turkeyNode addChild:sprite];
+}
+
+- (SKSpriteNode *)randomize:(SKSpriteNode *)sprite {
+    sprite.position = CGPointMake((self.size.width) * ((float)rand()/RAND_MAX), (self.size.height) * ((float)rand()/RAND_MAX));
+    sprite.scale = .15;
+    sprite.alpha = 0;
+    sprite.color = [SKColor colorWithCGColor:[[UIColor colorWithRed:1*((float)rand()/RAND_MAX) green:1*((float)rand()/RAND_MAX)blue:1*((float)rand()/RAND_MAX) alpha:1.0] CGColor]];
+    sprite.colorBlendFactor = 1.0;
+    
+    NSTimeInterval duration = 0.25 * ((float)rand()/RAND_MAX) + 0.75;
+    SKAction *effects = [SKAction group:@[
+                                          [SKAction fadeAlphaTo:0.05 duration:duration],
+                                          [SKAction scaleTo:0 duration:duration],
+                                          [SKAction rotateToAngle:((float)rand()/RAND_MAX)*(M_PI/3)-M_PI/6 duration:duration shortestUnitArc:true]
+                                          ]];
+    
+    [sprite runAction:[SKAction sequence:@[
+        [SKAction runBlock:^{
+            counter++;
+        }],
+        [SKAction fadeInWithDuration:0],
+        effects,
+        [SKAction runBlock:^{
+            BOOL shouldRemove = (int)[[NSDate date] timeIntervalSince1970] % 2 == 0;
+            if (!shouldDraw || (turkeyCount > targetTurkeyCount && shouldRemove)) {
+                [sprite removeFromParent];
+                turkeyCount--;
+            } else {
+                [self randomize:sprite];
+            }
+        }]
+    ]]];
+    
+    return sprite;
 }
 
 -(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
+    if (counter > 0) {
+        labelNode.text = [NSString stringWithFormat:@"%i turkeys!", counter];
+    }
+}
+
+- (void)displayGestureForPanRecognizer:(UIPanGestureRecognizer *)recognizer {
+    targetTurkeyCount+= (int)([recognizer velocityInView:self.view].y * -1 / 100);
+    if (targetTurkeyCount < 10) {
+        targetTurkeyCount = 10;
+    }
+    
+    labelTurkeyCount.text = [NSString stringWithFormat:@"%i tps", targetTurkeyCount];
+}
+
+- (void)displayGestureForTapRecognizer:(UITapGestureRecognizer *)recognizer {
+    shouldDraw = !shouldDraw;
+    [self addTurkeyNode];
+}
+
+- (void)didMoveToView:(SKView *)view {
+    UIPanGestureRecognizer *panRecognizer =[[UIPanGestureRecognizer alloc] initWithTarget:self action:(@selector(displayGestureForPanRecognizer:))];
+    panRecognizer.maximumNumberOfTouches = 1;
+    panRecognizer.minimumNumberOfTouches = 1;
+    [view addGestureRecognizer:panRecognizer];
+    
+    UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:(@selector(displayGestureForTapRecognizer:))];
+    tapRecognizer.numberOfTapsRequired = 2;
+    [view addGestureRecognizer:tapRecognizer];
 }
 
 @end
